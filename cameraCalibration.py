4d9784d398d387efba84f6145e9e9870c5e9b877
thlstfs <38380519+thlstfs@users.noreply.github.com>
2020-06-22 10:39:59 +0300

    v0.01

--- cameraCalibration.py
+++ cameraCalibration.py
@@ -1,41 +1,51 @@
 import cv2 as cv
 import numpy as np
 
-# termination criteria
-criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
-# Arrays to store object points and image points from all the images.
-objpoints = [] # 3d point in real world space
-imgpoints = [] # 2d points in image plane.
+def setCamResolution(cap, width, height):
+    cap.set(3,width)
+    cap.set(4,height)
 
-ret1, mtx, dist, rvecs, tvecs = False, None, None, None, None
-
-cap = cv.VideoCapture(0)
-
-while(True):
-    ret, frame = cap.read()
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    # Find the chess board corners
-    ret, corners = cv.findChessboardCorners(gray, (7,6), None)
-    # If found, add object points, image points (after refining them)
-    print(corners)
-    if ret == True:
-        objpoints.append(objp)
-        corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
-        imgpoints.append(corners)
-        # Draw and display the corners
-        cv.drawChessboardCorners(frame, (7,6), corners2, ret)
-        ret1, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-        # print([ret, mtx, dist, rvecs, tvecs])
-    cv.imshow('img', frame)
-    # print(ret1, mtx, dist, rvecs, tvecs)
+def cameraCalibrate(camId, width, height, output):
+    # termination criteria
+    criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+    objp = np.zeros((6*7,3), np.float32)
+    objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+    # Arrays to store object points and image points from all the images.
+    objpoints = [] # 3d point in real world space
+    imgpoints = [] # 2d points in image plane.
+    
+    ret1, mtx, dist, rvecs, tvecs = False, None, None, None, None
+    
+    cap = cv.VideoCapture(camId)
+    setCamResolution(cap, width, height)
+    
+    while(True):
+        ret, frame = cap.read()
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        # Find the chess board corners
+        ret, corners = cv.findChessboardCorners(gray, (7,6), cv.CALIB_CB_ADAPTIVE_THRESH + cv.CALIB_CB_FAST_CHECK + cv.CALIB_CB_NORMALIZE_IMAGE)
+        # If found, add object points, image points (after refining them)
+        print(corners)
+        if ret == True:
+            objpoints.append(objp)
+            corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
+            imgpoints.append(corners)
+            # Draw and display the corners
+            cv.drawChessboardCorners(frame, (7,6), corners2, ret)
+            # print([ret, mtx, dist, rvecs, tvecs])
+        cv.imshow('img', frame)
+        # print(ret1, mtx, dist, rvecs, tvecs)
+        if cv.waitKey(1) & 0xFF == ord('q'):    
+            break
+    
+    cap.release()
+    cv.destroyAllWindows()
+    ret1, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+            
     if (ret1):
-        np.savez("cam", mtx = mtx, dist = dist, rvecs = rvecs, tvecs = tvecs)
-        break
-    if cv.waitKey(1) & 0xFF == ord('q'):    
-        break
-
-cap.release()
-cv.destroyAllWindows()
+        np.savez(output, mtx = mtx, dist = dist, rvecs = rvecs, tvecs = tvecs)
+        
+    print("done")
+    
+cameraCalibrate(0, 960, 540, "cam")
--- main.py
+++ main.py
@@ -1,54 +1,175 @@
 import pupil_apriltags as at
 import cv2 as cv
 import numpy as np
-at_detector = at.Detector(families='tag36h11',
-                       nthreads=1,
-                       quad_decimate=1.0,
-                       quad_sigma=0.0,
-                       refine_edges=1,
-                       decode_sharpening=0.25,
-                       debug=0)
+import threading
 
+detectedTag = None
 
-cap = cv.VideoCapture(0)
-with np.load('cam.npz') as X:
-    mtx, dist, rvecs, tvecs = [X[i] for i in ('mtx','dist','rvecs','tvecs')]
-
-def drawPoly(img, pointsArray):
-    length = len(pointsArray)
+def drawBorders(img, corners):
+    length = len(corners)
     for i in range(-1, length - 1):
-        x1, y1 = pointsArray[i][0], pointsArray[i][1]
-        x2, y2 = pointsArray[i+1][0], pointsArray[i+1][1]
-        cv.line(frame,(int(x1),int(y1)),(int(x2),int(y2)),(0,255 * ((i + 1) % 2),255 * ((i + 2) % 2)),2)
+        x1, y1 = int(corners[i][0]), int(corners[i][1])
+        x2, y2 = int(corners[i+1][0]), int(corners[i+1][1])
+        cv.line(img,(x1,y1),(x2,y2),(0,255 * ((i + 1) % 2),255 * ((i + 2) % 2)),2)
+
+def cam1():
+    global detectedTag
+    at_detector = at.Detector(families='tag36h11',
+                           nthreads=1,
+                           quad_decimate=1.0,
+                           quad_sigma=0.0,
+                           refine_edges=1,
+                           decode_sharpening=0.25,
+                           debug=0)
+    
+    
+    cap = cv.VideoCapture(0)
+    # setCamResolution(cap, 960, 540)
+    with np.load('cam0.npz') as X:
+        mtx, dist, rvecs, tvecs = [X[i] for i in ('mtx','dist','rvecs','tvecs')]
+   
+    
+    while(True):
+        # print(rvecs)
+        ret, frame = cap.read()
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        tags = at_detector.detect(gray, estimate_tag_pose=True, camera_params=[mtx[0][0],mtx[1][1], mtx[0][2], mtx[1][2]], tag_size=0.182)
+        #print(len(frame))
+        if (tags):
+            for tag in tags:
+                detectedTag = tag
+                print(tag)
+                drawBorders(frame, tag.corners)
+                rotX, rotY, rotZ = getRotationInfo(tag.pose_R)
+                print("Rotation: ", rotX, ",", rotY, ",", rotZ)
+                print(tag.pose_t)
+                print("------------------------------------------------------------")
+        
+        cv.imshow('frame',frame)
+        if cv.waitKey(1) & 0xFF == ord('q'):    
+            break
+    
+    cap.release()
+    cv.destroyAllWindows()
+    
+    
+def cam2():
+    global detectedTag
+    cap = cv.VideoCapture(3)
+    setCamResolution(cap, 1920, 1080)
+    with np.load('cam1.npz') as X:
+        mtx, dist, rvecs, tvecs = [X[i] for i in ('mtx','dist','rvecs','tvecs')]
+    while(True):
+        ret, frame = cap.read()
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        
+        data, bbox, _ = detectQRcode(gray)
+        focusTheTag(data)        
+        # if (not data):
+        #     print("zoom in")
+        cv.imshow('frame1',frame)
+        if cv.waitKey(1) & 0xFF == ord('q'):    
+            break
+    
+    cap.release()
+    cv.destroyAllWindows()
+    
+def cam3():
+    global detectedTag
+    at_detector = at.Detector(families='tag36h11',
+                           nthreads=1,
+                           quad_decimate=1.0,
+                           quad_sigma=0.0,
+                           refine_edges=1,
+                           decode_sharpening=0.25,
+                           debug=0)
+    
+    
+    cap = cv.VideoCapture(3)
+    setCamResolution(cap, 1920, 1080)
+    with np.load('cam1.npz') as X:
+        mtx, dist, rvecs, tvecs = [X[i] for i in ('mtx','dist','rvecs','tvecs')]
+   
+    
+    while(True):
+        # print(rvecs)
+        ret, frame = cap.read()
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        tags = at_detector.detect(gray, estimate_tag_pose=True, camera_params=[mtx[0][0],mtx[1][1], mtx[0][2], mtx[1][2]], tag_size=0.182)
+        #print(len(frame))
+        if (tags):
+            for tag in tags:
+                detectedTag = tag
+                print(tag)
+                drawBorders(frame, tag.corners)
+                rotX, rotY, rotZ = getRotationInfo(tag.pose_R)
+                print("Rotation: ", rotX, ",", rotY, ",", rotZ)
+                print(tag.pose_t)
+                print("------------------------------------------------------------")
+        
+        cv.imshow('frame3',frame)
+        if cv.waitKey(1) & 0xFF == ord('q'):    
+            break
+    
+    cap.release()
+    cv.destroyAllWindows()
+def setCamResolution(cap, width, height):
+    cap.set(3,width)
+    cap.set(4,height)
+
+def focusTheTag(data):
+    if (not data):
+        print("focusing")
+        return False
+    else:
+        return True
+
+def getRotationInfo(tagPoseR):
+    rotZ = tagPoseR[0][0] / tagPoseR[0][1]
+    rotZ = np.arctan(rotZ)
+    rotZ = np.rad2deg(rotZ)
+    rotY = np.arcsin(-tagPoseR[0][2])
+    rotY = np.rad2deg(rotY)
+    rotX = tagPoseR[1][2] / tagPoseR[2][2]
+    rotX= np.arctan(rotX)
+    rotX = np.rad2deg(rotX)
+    return rotX, rotY, rotZ
+    
+def detectQRcode(grayImg):
+    detector = cv.QRCodeDetector()
+    data, bbox, _ = detector.detectAndDecode(grayImg)
+    if data:
+        print("QR Code detected-->", data)
+    return data, bbox, _ 
+
+def getCamIDs():
+    index = 0
+    arr = []
+    while True:
+        cap = cv.VideoCapture(index)
+        if not cap.read()[0]:
+            break
+        else:
+            arr.append(index)
+        cap.release()
+        index += 1
+    print(arr)
+    return arr
 
 
-while(True):
-    # print(rvecs)
-    ret, frame = cap.read()
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    tags = at_detector.detect(gray, estimate_tag_pose=True, camera_params=[mtx[0][0],mtx[1][1], mtx[0][2], mtx[1][2]], tag_size=0.2)
-    # print(len(frame[0]))
-    if (tags):
-        for tag in tags:
-            drawPoly(frame, tag.corners)
-            rotZ = tag.pose_R[0][0] / tag.pose_R[0][1]
-            rotZ = np.arctan(rotZ)
-            rotZ = np.rad2deg(rotZ)
-            rotY = np.arcsin(tag.pose_R[0][2])
-            rotY = np.rad2deg(rotY)
-            rotX = tag.pose_R[1][2] / tag.pose_R[2][2]
-            rotX= np.arctan(rotX)
-            rotX = np.rad2deg(rotX)
-            print("Rotation X axis ", rotX, " degrees")
-            print("Rotation Y axis ", rotY, " degrees")
-            print("Rotation Z axis ", rotZ, " degrees")
-            print(tag.pose_t)
-            print("------------------------------------------------------------")
-                
-    cv.imshow('frame',frame)
-    # print(tags)
-    if cv.waitKey(1) & 0xFF == ord('q'):    
-        break
+if __name__ == "__main__":
+    getCamIDs()
 
-cap.release()
-cv.destroyAllWindows()
+    t1 = threading.Thread(target=cam1)
+    t2 = threading.Thread(target=cam2)
+    # starting thread 1 
+    t1.start() 
+    # starting thread 2 
+    t2.start() 
+  
+    # wait until thread 1 is completely executed 
+    t1.join() 
+    # wait until thread 2 is completely executed 
+    t2.join() 
+    # both threads completely executed 
+    print("Done!")
